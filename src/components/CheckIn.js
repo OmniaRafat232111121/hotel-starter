482a17e2c2372a307586e05349a4fe08da1bb048
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2023-02-25 18:17:20 +0200

    Kids,Adults Drop

--- src/components/CheckIn.js
+++ src/components/CheckIn.js
@@ -9,12 +9,20 @@ const CheckIn = () => {
   const [startDate,setStartDate]=useState(false);
   return(
     <>
-    <DatePicker
+   <div className='relative flex items-center h-full'>
+{/*icon*/}
+<div className='absolute  ml-2 z-100 pr-8'>
+  <div>
+    <BsCalendar className='text-accent text-base'/>
+  </div>
+</div>
+<DatePicker
     className='w-full h-full'
     selected={startDate}
     placeholderText='Check in'
     onChange={(date)=>setStartDate(date)}
     />
+   </div>
 
     </>
   )
--- src/context/RoomContext.js
+++ src/context/RoomContext.js
@@ -5,10 +5,15 @@ import {roomData} from '../data'
 export const RoomContext=createContext();
 const RoomProvider=({children})=>{
   const [rooms,setRooms]=useState(roomData);
+  const [adults,setAdults]=useState(' 1 Adult');
+  const [kids,setKids]=useState(' 0 Kids');
+  const [total,setTotal]=useState(0);
+
   console.log(rooms)
+  console.log(`adult is${adults} ,Kids is ${kids}`)
   return(
     <div>
-      <RoomContext.Provider value={{rooms}}>
+      <RoomContext.Provider value={{rooms,adults,setAdults,kids,setKids}}>
         {children}
       </RoomContext.Provider>
 
